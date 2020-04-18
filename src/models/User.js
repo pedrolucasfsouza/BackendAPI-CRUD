4313d89a363b6e4f01353db9153f6cb978133bb4
Pedro Lucas <pedrolucasfsouza@gmail.com>
2020-04-18 18:01:49 -0300

    validation models for mongodb

--- src/models/User.js
+++ src/models/User.js
@@ -4,14 +4,19 @@ const UserSchema = new mongoose.Schema({
   userName: {
     type: String,
     require: true,
+    minlength: 5,
+    maxlength: 30,
   },
   email: {
     type: String,
     required: true,
+    minlength: 10,
+    maxlength: 45,
   },
   password: {
     type: String,
     required: true,
+    minlength: 5,
   },
 });
 
